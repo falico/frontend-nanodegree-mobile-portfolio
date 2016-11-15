c382bb8d25f7d08f2257ca50769e227e500e22bc
falico <falico@gmail.com>
2016-11-15 14:46:40 -0800

    adjusting performance pizzaria

--- views/js/main.js
+++ views/js/main.js
@@ -505,7 +505,11 @@ function updatePositions() {
   var itemsLength = items.length;
   for (var i = 0; i < itemsLength; i++) {
     var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    var left = items[i].basicLeft + 100 * phase + 'px';
+    // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    // items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.transform = "translateX("+-left+") translateZ(0)";
+
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
